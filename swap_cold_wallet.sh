a77fe2449cfb6d906f8ddaf63ffcf557c7bd189b
CryptoCatOkiOki <34884057+CryptoCatOkiOKi@users.noreply.github.com>
2018-09-21 13:37:18 +0200

    MC coin - swap cold wallet to new chain
    MC coin - swap cold wallet to new chain

--- swap_cold_wallet.sh
+++ swap_cold_wallet.sh
@@ -4,6 +4,8 @@ echo
 echo "Welcome to the MASTERCOIN Masternode update script."
 echo
 
+IP=$(hostname  -I | cut -f1 -d' ')
+
 cd ~
 sudo killall -9 mastercoind
 sudo rm -rdf /usr/bin/mastercoind 
@@ -24,8 +26,12 @@ rm -d MASTERCOINONE_TMP
 mkdir -p ~/.mastercoin
 cp ~/.MasterCoinV2/mastercoin.conf ~/.mastercoin
 
+sed -i 's/masternodeaddress/#masternodeaddress/g' ~/.mastercoin/mastercoin.conf
 echo "rpcallowip=127.0.0.1" >> ~/.mastercoin/mastercoin.conf
 echo "rpcport=17301" >> ~/.mastercoin/mastercoin.conf
+echo "masternodeaddress=$IP" >> ~/.mastercoin/mastercoin.conf
+
+ufw allow 17301
 
 mastercoind
 
